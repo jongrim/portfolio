6301839609b65fd703a335f99c4f28276c9f6ae5
Jonathan Grim <jonjongrim@gmail.com>
2017-06-25 16:06:57 -0400

    Update styling and sections

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,65 @@
 var App = (function() {
   var livelyImages = ['./assets/images/Lively/Lively.png', './assets/images/Lively/Lively2.jpeg'];
+  var customerImages = ['./assets/images/Customer/Home.jpeg', './assets/images/Customer/Search.jpeg'];
+
+  var $customerProjectLink, $customerProjectDiv, $livelyProjectLink, $livelyProjectDiv;
+
   function init() {
-    let currentPhoto = 0;
-    setInterval(function() {
-      let $livelyImage = $('.lively');
-      $livelyImage.fadeOut(600, function() {
-        $livelyImage.attr('src', function() {
-          if (currentPhoto === livelyImages.length - 1) {
-            currentPhoto = 0;
-          } else {
-            currentPhoto++;
-          }
-          return livelyImages[currentPhoto];
-        });
-        $livelyImage.fadeIn();
-      });
-    }, 5000);
+    $customerProjectLink = $('#customerProjectLink');
+    $livelyProjectLink = $('#livelyProjectLink');
+    $customerProjectDiv = $('#customerProjectInfo');
+    $livelyProjectDiv = $('#livelyProjectInfo');
+    let livelyCurrentPhoto = 0;
+
+    // setInterval(function() {
+    //   let $livelyImage = $('.lively');
+    //   $livelyImage.fadeOut(600, function() {
+    //     $livelyImage.attr('src', function() {
+    //       if (livelyCurrentPhoto === livelyImages.length - 1) {
+    //         livelyCurrentPhoto = 0;
+    //       } else {
+    //         livelyCurrentPhoto++;
+    //       }
+    //       return livelyImages[livelyCurrentPhoto];
+    //     });
+    //     $livelyImage.fadeIn();
+    //   });
+    // }, 5000);
+
+    // let custCurrentPhoto = 0;
+
+    // setInterval(function() {
+    //   let $customerImage = $('.customer');
+    //   $customerImage.fadeOut(600, function() {
+    //     $customerImage.attr('src', function() {
+    //       if (custCurrentPhoto === customerImages.length - 1) {
+    //         custCurrentPhoto = 0;
+    //       } else {
+    //         custCurrentPhoto++;
+    //       }
+    //       return customerImages[custCurrentPhoto];
+    //     });
+    //     $customerImage.fadeIn();
+    //   });
+    // }, 5000);
+
+    $customerProjectLink.on('click', toggleCustomerProject);
+    $livelyProjectLink.on('click', toggleLivelyProject);
+  }
+
+  function toggleCustomerProject(evt) {
+    evt.preventDefault();
+    evt.stopPropagation();
+    evt.stopImmediatePropagation();
+
+    $customerProjectDiv.slideToggle();
+  }
+  function toggleLivelyProject(evt) {
+    evt.preventDefault();
+    evt.stopPropagation();
+    evt.stopImmediatePropagation();
+
+    $livelyProjectDiv.slideToggle();
   }
 
   return {
